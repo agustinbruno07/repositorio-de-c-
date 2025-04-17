80bba4eb6c3ae571bf1873fdc3fa714e876f8d7e
Agus <brunoagustin562@gmail.com>
2025-04-17 12:41:39 -0300

    n

---
+++ TP2/act8/actividad8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+char palabras[5][30],
+int;
+main(){
+    for(i=0;i<n;i++){
+    	printf("ingrese la palabra %d\n", i++);
+    	fgets(palabra, sizeof(palabra), stdin);
+    	if(palabra[i][strlen(palabra[i]) - 1 ] == '\n'){
+    		palabra[i][strlen(palabra[i]) - 1 ] = '\0';
+		}
+	}
+	
+	
+	
+	
+	
+}//main
