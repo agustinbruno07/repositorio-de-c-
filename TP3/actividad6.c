f4f7086d41f2954b711b74dfdb2e850b3ee8cce3
Agus <brunoagustin562@gmail.com>
2025-04-19 20:31:25 -0300

    s

--- TP3/actividad6.c
+++ TP3/actividad6.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
-int i=0, numeros[10], n;
-void ocuerrencias()
-main(){
-	printf("ingrese el tamaño del arreglo");
-	scanf("%d", &n );
-	for(i=0;i<n;i++){
-		printf("ingrese de la posicion %d el numero:", i);
-		scanf("%d", &numeros[i]);
-	}
-	
-	
-}//main
+
+int arreglo[100], elemento, cantidad, tamanio;
+int contarOcurrencias(int *arr, int elem, int tam);
+main() {
+    int i;
+    
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &tamanio);
+    
+    printf("Ingrese los elementos del arreglo:\n");
+    for(i = 0; i < tamanio; i++) {
+        scanf("%d", &arreglo[i]);
+    }
+    
+    printf("Ingrese el elemento a buscar: ");
+    scanf("%d", &elemento);
+    
+    cantidad = contarOcurrencias(arreglo, elemento, tamanio);
+    
+    printf("El elemento %d aparece %d veces.\n", elemento, cantidad);
+    
+    return 0;
+}
+
+int contarOcurrencias(int *arr, int elem, int tam) {
+    int contador = 0;
+    int i;
+    
+    for(i = 0; i < tam; i++) {
+        if(*arr == elem) {
+            contador++;
+        }
+        arr++;
+    }
+    
+    return contador;
+}
+
